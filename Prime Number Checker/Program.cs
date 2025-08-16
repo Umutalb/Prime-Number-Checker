73b9863002ab0d9f106364cc85f69c8ba8f8abfc
Umutalb <23390008061@yyu.edu.tr>
2025-08-16 23:54:46 +0300

    Proje dosyası ekle.

---
+++ Prime Number Checker/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Prime_Number_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Prime Number Checker-----\n");
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+            int number;
+
+            if (int.TryParse(input, out number))
+            {
+                if (number < 2)
+                {
+                    Console.WriteLine("Not Prime");
+                }
+                else if (number == 2)
+                {
+                    Console.WriteLine("Prime");
+                }
+                else if (number % 2 == 0)
+                {
+                    Console.WriteLine("Not Prime");
+                }
+                else
+                {
+                    bool isPrime = true;
+                    for (int i = 3; i <= Math.Sqrt(number); i += 2)
+                    {
+                        if (number % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    if (isPrime)
+                        Console.WriteLine("Prime");
+                    else
+                        Console.WriteLine("Not Prime");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number!");
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
